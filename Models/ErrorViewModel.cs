c23541de72825db9be7018c51c604e649bc464ed
CasLeeflang <466933@student.fontys.nl>
2021-03-08 10:38:24 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IndividueelProject_CLeeflang.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
